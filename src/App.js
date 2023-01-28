548bcb4de2389e93d528d213e6433b73a0c994c6
shawki285 <shawky830@gmail.com>
2023-01-28 22:37:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './Cart';
+import { commerce } from './Ecommerce';
+import Home from './Home';
+import NavBar from './NavBar';
+import Shop from './Shop';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+const App = () => {
 
-function App() {
+  const[products , setProducts ] = useState([]);
+
+  const [cart, SetCart] = useState([]);
+
+
+  const fetchProducts = async () =>{
+    const { data } = await commerce.products.list();
+    setProducts(data)
+  };
+
+  const fetchCart = async () =>{
+    SetCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddToCart = async (productId, quantity) =>{
+    const item = await commerce.cart.add(productId, quantity)
+    SetCart(item.cart);
+  }
+
+  const handleUpdateCartQty = async (lineItemId, quantity) =>{
+    const response = await commerce.cart.update(lineItemId, {quantity});
+    SetCart(response.cart);
+  }
+
+  const handleRemoveFromCart =async (lineItemId) =>{
+    const response = await commerce.cart.remove(lineItemId);
+    SetCart(response.cart);
+
+  }
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+    SetCart(response.cart)
+  }
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+  console.log(products);
+  console.log(cart);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+      <div className='App'>
+      <NavBar  totalItems={cart.total_items}/>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/Shop' element={<Shop products={products} onAddToCart={handleAddToCart}/>} />
+           <Route path='/Cart' element={<Cart  cart={cart} onUpdateCartQty={handleUpdateCartQty}  onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />}  />
+
+        </Routes>
+      </div>
+   </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Shop.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Product from './Product';
+import { Col, Container, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import Footer from './Footer';
+const Shop = ({products, onAddToCart}) => {
+  return (
+    <div>
+      <div className="divteam">
+        <div>
+          <h3>Shop</h3>
+          <Link to="/" >Home </Link><span> / Shop</span>
+        </div>
+      </div>
+      <Container className="SHOPALLL">
+        <Row>
+          {products.map((product) => (
+            <Col md="4" sm="12" key={product.id}>
+              <Product product={product} onAddToCart={onAddToCart} /> 
+            </Col>
+          ))}
+        </Row>
+      </Container>
+  
+  
+      
+      <Footer />
+    </div>
+  )
+}
+
+export default Shop
